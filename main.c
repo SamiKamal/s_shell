c7d2d6c4898d1c90ba6aee7e97bb66b8df7cad98
Sami Ibrahim <sami-dev@hotmail.com>
2024-02-28 21:57:50 +0300

    feat: read command

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EXIT_SUCCESS 0
+
+#define DEFAULT_MAX_COMMAND 100
+
+char* read_command(char str[]);
+char* parse_command(char str[]);
+int main_loop();
 
 int main() {
-    printf("Hello World\n");
+    printf("> Shell: ");
 
-    return 0;
+    main_loop();
+
+    return EXIT_SUCCESS;
+}
+
+int main_loop() {
+    while (1) {
+        char* str = (char*)calloc(DEFAULT_MAX_COMMAND, sizeof(char));
+        read_command(str);
+        if (strcmp(str, "exit") == 0) {
+            return 0;
+        }
+        printf("> Shell: ");
+    }
+}
+
+char* read_command(char str[]) {
+    char ch = 0;
+    int i = 0;
+
+    while ((ch = getchar()) != '\n') {
+        // @TODO: handle when user goes over DEFUALT_MAX_COMMAND
+        str[i++] = ch;
+    }
+    str[i] = '\0';
+
+    return str;
 }
